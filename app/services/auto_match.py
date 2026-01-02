999e8609eac09846c91350fc5670a96abdb72c3f
Evgeny Lavrov <evgeny.lavrov2002@mail.ru>
2026-01-03 03:45:30 +0700

    Обновлена документация

--- app/services/auto_match.py
+++ app/services/auto_match.py
@@ -1,12 +1,15 @@
 from app.clients.lcu_api import Client
 
 from app.config import CONFIG_FILE, BASE_CONFIG_FILE, CHAMPIONS_FILE
+from app.services.updater import ProjectUpdater
 from app.utils import get_or_download_champions, get_champions_for_role_by_mastery, load_champions, get_current_queue, \
     get_role_name, find_available_champion, print_lobby
 from app.utils.get_champions_for_role import get_champions_for_role
 
 
 def main():
+    updater = ProjectUpdater()
+    updater.check_for_updates()
     print("Загрузка героев")
     ddragon_data = get_or_download_champions(CHAMPIONS_FILE)
     champions = load_champions(CONFIG_FILE, BASE_CONFIG_FILE, ddragon_data)
